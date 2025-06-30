788020054c36b83f14d3848ff74f8c18d59cf61c
Weelhz <113856729+Weelhz@users.noreply.github.com>
2025-07-01 00:21:52 +0200

    Update service-setup.sh

--- ubuntu-setup/service-setup.sh
+++ ubuntu-setup/service-setup.sh
@@ -1,20 +1,14 @@
-
 #!/bin/bash
 
-# Setup Inventoria as a system service
 echo "Setting up Inventoria as a system service..."
 
-# Create inventoria user
 sudo useradd --system --home /opt/inventoria --shell /bin/false inventoria
 
-# Copy application to /opt/inventoria
 sudo cp -r . /opt/inventoria/
 sudo chown -R inventoria:inventoria /opt/inventoria
 
-# Copy service file
 sudo cp ubuntu-setup/inventoria.service /etc/systemd/system/
 
-# Reload systemd and enable service
 sudo systemctl daemon-reload
 sudo systemctl enable inventoria
 
