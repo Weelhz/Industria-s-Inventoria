4641f8d2afaed2f5ef08dcd1bad2ccfb45e8ff65
Weelhz <113856729+Weelhz@users.noreply.github.com>
2025-06-30 23:13:51 +0200

    Update item-details-modal.tsx

--- client/src/components/inventory/item-details-modal.tsx
+++ client/src/components/inventory/item-details-modal.tsx
@@ -1,4 +1,3 @@
-
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
 import { Badge } from "@/components/ui/badge";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
